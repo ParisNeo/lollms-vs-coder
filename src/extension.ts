96707e2eb44c6ac53ae636b109d616362e674dc8
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2025-09-08 09:01:09 +0200

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,1 +1,14 @@
+import * as vscode from 'vscode';
 
+export function activate(context: vscode.ExtensionContext) {
+  console.log('Lollms VS Coder is now active!');
+
+  const startChatCommand = vscode.commands.registerCommand('lollms-vs-coder.startChat', () => {
+    vscode.window.showInformationMessage('Lollms chat started (placeholder)');
+    // TODO: Initialize chat panel and Lollms API interaction here
+  });
+
+  context.subscriptions.push(startChatCommand);
+}
+
+export function deactivate() {}
